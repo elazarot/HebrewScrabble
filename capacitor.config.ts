2412dcb457b7d5ae7503a59414203657ab8cf7e7
elazarot <elazarot88@gmail.com>
2026-05-29 19:16:18 +0300

    config: disable live reload for static native android installation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.hebrewscrabble.app',
   appName: 'שבץ נא',
-  webDir: 'dist',
-  server: {
-    url: 'http://192.168.1.201:3000',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
